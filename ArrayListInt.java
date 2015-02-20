78768cf7aff1657222375dc0ef9880a66bf74714
Julia Zuara <julia_any@hotmail.com>
2015-02-20 12:18:36 +0100

    Añadido el metodo add para añadir un numero como el ultimo de la lista

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -15,7 +15,27 @@ public class ArrayListInt
      */
     public ArrayListInt()
     {
-        int[] lista;
+        // Inicializamos con un valor 0 posiciones para no desperdiciar
+        // espacio en ningun momento
+        lista = new int[0];
     }
 
+    /**
+     * Metodo que añade un elemento a la arrayList
+     */
+    public void add(int numero)
+    {
+        // Creamos una lista temporal, transpasamos todos los numeros con un
+        // for, y añadimos el numero en la ultima posicion
+        int tamaño = lista.length;
+        int[] temporal = new int[tamaño + 1];
+        for(int i = 0; i < (tamaño - 1); i++)
+        {
+            temporal[i] = lista[i];
+        }
+        temporal[lista.length] = numero;
+        // Por ultimo pasamos la lista temporal a lista
+        lista = temporal;
+
+    }
 }
